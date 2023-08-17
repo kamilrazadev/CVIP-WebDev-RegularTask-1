0812b054d07081c732d4b3642fbd54686305d67e
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-08-18 00:15:25 +0500

    first commit

--- Components/ProductCard.js
+++ Components/ProductCard.js
@@ -10,24 +10,70 @@ const Laptops = [
         productPrice: "$109"
     },
     {
-        productImage:  "./assets/product-images/laptops/1.png", 
-        productTitle: "HP Probook i5 10th Gen",
-        productPrice: "$109"
+        productImage:  "./assets/product-images/laptops/3.png", 
+        productTitle: "HP Probook i7 5th Gen",
+        productPrice: "$178"
     },
     {
-        productImage:  "./assets/product-images/laptops/1.png", 
+        productImage:  "./assets/product-images/laptops/4.webp", 
         productTitle: "HP Probook i5 10th Gen",
-        productPrice: "$109"
+        productPrice: "$119"
     },
     {
-        productImage:  "./assets/product-images/laptops/1.png", 
-        productTitle: "HP Probook i5 10th Gen",
-        productPrice: "$109"
-    },
-    {
-        productImage:  "./assets/product-images/laptops/1.png", 
-        productTitle: "Haroon Laptop",
+        productImage:  "./assets/product-images/laptops/5.png", 
+        productTitle: "Acer Laptop",
         productPrice: "$219"
+    }
+]
+
+const Mobiles = [
+    {
+        productImage:  "./assets/product-images/mobiles/1.webp", 
+        productTitle: "iPhone 14 Pro Max",
+        productPrice: "$199"
+    },
+    {
+        productImage:  "./assets/product-images/mobiles/2.webp", 
+        productTitle: "Redmi Note 9S",
+        productPrice: "$109"
+    },
+    {
+        productImage:  "./assets/product-images/mobiles/3.png", 
+        productTitle: "iPhone 11",
+        productPrice: "$119"
+    },
+    {
+        productImage:  "./assets/product-images/mobiles/4.webp", 
+        productTitle: "iPhone XS Max",
+        productPrice: "$78"
+    },
+    {
+        productImage:  "./assets/product-images/mobiles/5.jpg", 
+        productTitle: "One Plus Nord 200 5G",
+        productPrice: "$89"
+    }
+]
+
+const Tablets = [
+    {
+        productImage:  "./assets/product-images/tablets/1.png", 
+        productTitle: "iPad",
+        productPrice: "$109"
+    },
+    {
+        productImage:  "./assets/product-images/tablets/2.webp", 
+        productTitle: "Samsung Galaxy Tab S9 Ultra",
+        productPrice: "$159"
+    },
+    {
+        productImage:  "./assets/product-images/tablets/3.avif", 
+        productTitle: "Lenove Tab P12",
+        productPrice: "$65"
+    },
+    {
+        productImage:  "./assets/product-images/tablets/4.webp", 
+        productTitle: "Realme Pad 2",
+        productPrice: "$78"
     }
 ]
 
@@ -48,13 +94,21 @@ function ProductCard(product) {
 }
 
 function displayProducts() {
-    const ProductContainer = document.getElementById('products-container');
+    const laptopContainer = document.getElementById('laptops-container');
+    const mobileContainer = document.getElementById('mobiles-container');
+    const tabletsContainer = document.getElementById('tablets-container');
 
-    const productHTMLArray = Laptops.map(laptop => ProductCard(laptop));
 
-    console.log(productHTMLArray)
+    const laptopHTMLArray = Laptops.map(laptop => ProductCard(laptop));
+    const mobileHTMLArray = Mobiles.map(mobile => ProductCard(mobile));
+    const tabletsHTMLArray = Tablets.map(tablet => ProductCard(tablet));
 
-    ProductContainer.innerHTML = productHTMLArray.join('');
+
+    laptopContainer.innerHTML = laptopHTMLArray.join('');
+    mobileContainer.innerHTML = mobileHTMLArray.join('');
+    tabletsContainer.innerHTML = tabletsHTMLArray.join('');
+
+
 }
 
 // Call the displayProducts function to generate and display the product cards
